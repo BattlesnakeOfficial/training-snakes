5f122e39bafbe17a014a41f8bbb5e6ac03a8499d
John Oram <john@oram.ca>
2018-02-23 17:16:59 -0800

    ...

--- app/main.py
+++ app/main.py
@@ -33,6 +33,8 @@ def move(snake_name):
     gamestate = snake.payload_to_game_state(data)
     move = snake.move(gamestate)
     if move is None:
+        import pprint
+        pprint.pprint(data)
         return json.dumps({"move": "up"})
 
     if type(move) is tuple:
